cc5c9c8a37ad75913b8208d15986cc92f2cf575d
rodapunk <estevanmachry@gmail.com>
2024-01-01 22:01:14 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, obj);
 		
 		System.out.println(obj);
+		System.out.println(seller);
 	}
 }
