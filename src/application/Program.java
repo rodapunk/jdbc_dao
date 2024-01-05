d25f7321baa11c632e384e1a1937b48a06b3f09d
rodapunk <estevanmachry@gmail.com>
2024-01-04 22:19:59 -0300

    DepartmentDaoJDBC implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -44,7 +44,7 @@ public class Program {
 		System.out.println("Update completed");
 		
 		System.out.println("\n=== TEST 6: seller delete ===");
-		System.out.println("Enter id for delete teste: ");
+		System.out.println("Enter id for delete test: ");
 		int id = sc.nextInt();
 		sellerDao.deleteById(id);
 		System.out.println("Delete completed");
